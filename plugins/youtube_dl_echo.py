2853eedba23f122b24090de517ea61a436c78427
๓คгςยร <65294906+prgofficial@users.noreply.github.com>
2021-04-01 21:58:43 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -26,7 +26,7 @@ from helper_funcs.help_uploadbot import DownLoadFile
 
 @Client.on_message(filters.private & filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
-    if update.from_user.id in Config.BANNED_USERS:
+    if update.from_user.id not in Config.AUTH_USERS:
         await bot.delete_messages(
             chat_id=update.chat.id,
             message_ids=update.message_id,
